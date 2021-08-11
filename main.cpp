5daefcdd4853c84c9f45cdcd8f9a827654e8aa17
arasmussen64 <arasmussen64@gmail.com>
2021-08-11 12:46:03 -0600

    Game intro

--- main.cpp
+++ main.cpp
@@ -1,5 +1,15 @@
 #include <iostream>
 
 int main() {
-    
+    //Intro to the game
+    std::cout << "***********************************\n";
+    std::cout << "Welcome to the Journey To The gods!\n";
+    std::cout << "***********************************\n\n";
+    std::cout << "Your goal is to escape a crumbling world of evil and destruction and find your place among the gods of old.\n";
+    std::cout << "Should you fail you will be destroyed...\n";
+    std::cout << "But if you can survive until the end your reward will be great!\n";
+
+    //Beginning of users choices 
+    int usersChoice;
+
 }
