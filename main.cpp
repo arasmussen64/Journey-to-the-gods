a242749db535d50a9ed82092e4b3c51c9e5b0ea5
arasmussen64 <arasmussen64@gmail.com>
2021-08-20 13:04:53 -0600

    quick update

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 
 int main() {
     //Intro to the game
@@ -7,9 +8,21 @@ int main() {
     std::cout << "***********************************\n\n";
     std::cout << "Your goal is to escape a crumbling world of evil and destruction and find your place among the gods of old.\n";
     std::cout << "Should you fail you will be destroyed...\n";
-    std::cout << "But if you can survive until the end your reward will be great!\n";
+    std::cout << "But if you can survive until the end your reward will be great!\n\n";
 
     //Beginning of users choices 
+    //std::vector<char> pack;
     int usersChoice;
 
+    std::cout << "Savage beasts have just overthrown the last human run government building and you need to run to the woods.\n";
+    std::cout << "What will you grab to take with you?\n";
+    std::cout << "A) Flashlight     B) Knife\n";
+    std::cout << "C) Tape Measurer  D) A can of beans\n";
+
+    std::vector<std::string> pack;
+
+    pack = {"Flashlight", "Knife"};
+    
+
+    std::cout << pack[1] << "\n";
 }
